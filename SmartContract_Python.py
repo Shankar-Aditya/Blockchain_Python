29783d662ef1e02da958228e1b134df2b4cec002
Shankar-Aditya <50665969+Shankar-Aditya@users.noreply.github.com>
2020-06-14 18:43:49 +0530

    Update SmartContract_Python.py

--- SmartContract_Python.py
+++ SmartContract_Python.py
@@ -6,9 +6,9 @@ web3 = Web3(Web3.HTTPProvider(ganache_url))
 print(web3.isConnected())
 
 #Ganache addresses and private key of one of these address
-account_1 = "0xc8b5486fCA46F48f75c3FFab06e06Eb32ba53aD3"
-account_2 = "0x44922678CF542154F0fa72570ef62aEBbD35aD04"
-private_key = "370fcc2767731a26af92de33a3306ac94707442a447901e4e8bc6e60385a0bdf"
+account_1 = "##########################################"
+account_2 = "******************************************"
+private_key = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 #get nonce-prevents two or more transcations
 nonce = web3.eth.getTransactionCount(account_1)
 #Building transaction btw accounts
